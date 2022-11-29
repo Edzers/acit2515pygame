2c58dc9f09e13cf9524704749557142149d0e673
Eddie <eddie0906@hotmail.com>
2022-11-29 01:09:25 -0800

    Scaled the background images, fixed it so screens / events work properly

--- screens/mainscreen.py
+++ screens/mainscreen.py
@@ -12,6 +12,8 @@ class Basescreen:
         #Load background images
         self.sky_surface = pygame.image.load("graphics/sky.jpg")
         self.ground_surface = pygame.image.load("graphics/ground.png")
+        self.sky_surface1 = pygame.transform.scale(self.sky_surface, (1000,800))
+        self.ground_surface1 = pygame.transform.scale(self.ground_surface, (1000, 800))
 
         #Set up fonts
 
@@ -27,15 +29,15 @@ class Basescreen:
             self.update()
             self.draw()
             pygame.display.update()
-            
 
             # Main event loop for the game
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.running = False
+                    self.running = False 
                     self.next_screen = False
                 elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     self.running = False
                     self.next_screen = False
                     
+                self.manage_event(event)
         
