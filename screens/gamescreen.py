79974a81f9693231b97acfc84d108a4e99e027d8
Eddie <eddie0906@hotmail.com>
2022-11-29 01:08:44 -0800

    Added background to the main screen

--- screens/gamescreen.py
+++ screens/gamescreen.py
@@ -6,7 +6,23 @@ class GameScreen(Basescreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        #Add sprites to a group
         self.sprites = pygame.sprite.Group()
         self.knuckles = Knuckles()
+
+    def update(self):
+        self.sprites.update()
+
+
+    def draw(self):
+        #Draw the sprites
+        self.sprites.draw(self.window)
+        self.window.fill((255, 255, 255))
+        #Blit background
+        self.window.blit(self.sky_surface1, (0,0))
+        self.window.blit(self.ground_surface1, (0,300))
+
+    def manage_event(self, event):
+        pass
 
 
