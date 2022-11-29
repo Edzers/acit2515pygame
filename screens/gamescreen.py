4470d3f983f1565f3bb750a629e4fa0080c691d0
Eddie <eddie0906@hotmail.com>
2022-11-29 02:09:13 -0800

    Added mainchar sprite image

--- screens/gamescreen.py
+++ screens/gamescreen.py
@@ -1,26 +1,30 @@
 import pygame
 from screens.mainscreen import Basescreen
 from components.knuckles import Knuckles
+from components.mainchar import Mainchar
 
 class GameScreen(Basescreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         #Add sprites to a group
-        self.sprites = pygame.sprite.Group()
         self.knuckles = Knuckles()
-
+        self.mainchar = Mainchar()
+        self.sprites = pygame.sprite.Group()
+        self.sprites.add(self.knuckles)
+        self.sprites.add(self.mainchar)
+    
     def update(self):
         self.sprites.update()
 
-
     def draw(self):
         #Draw the sprites
-        self.sprites.draw(self.window)
         self.window.fill((255, 255, 255))
         #Blit background
         self.window.blit(self.sky_surface1, (0,0))
         self.window.blit(self.ground_surface1, (0,300))
+        self.sprites.draw(self.window)
+
 
     def manage_event(self, event):
         pass
